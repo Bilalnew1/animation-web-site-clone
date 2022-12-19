47ba046f40c88d7b81530cc9db7d5cfed171cebe
Bilalnew1 <120711301+Bilalnew1@users.noreply.github.com>
2022-12-19 16:44:11 +0500

    Add files via upload

---
+++ app/src/App.jsx
@@ -0,0 +1,21 @@
+import New from "./components/new";
+import Nexttoherosection from "./components/nexttoherosection";
+import Mousehover from "./components/mousehover";
+import "./App.css"
+
+function App() {
+  return (
+    <>
+   <div className="main">
+
+      <New />
+      <Nexttoherosection/>
+      {/* <Mousehover/> */}
+   </div>
+    </>
+  );
+}
+
+export default App;
+
+
---
+++ app/src/components/nexttoherosection.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "../../src/App.css";
+
+const nexttoherosection = (props) => {
+  return (
+    <>
+     <div className="nextmain">
+{/* <div> */}
+    <br /><br />
+    <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit fugiat nam nulla minus labore delectus iure omnis doloribus reiciendis molestias commodi harum officia eveniet, expedita in rem quod eaque? Nulla!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis autem sint eveniet obcaecati molestiae voluptates est accusantium saepe facilis at, neque reiciendis officia aliquid, quisquam provident optio? Soluta, eos odit!
+        
+    </p>
+
+     </div>
+    </>
+  );
+};
+
+export default nexttoherosection;
